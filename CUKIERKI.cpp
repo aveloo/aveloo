7e3271fd610f60ac85f1f4087eea71c8270f01da
aveloo <70170748+aveloo@users.noreply.github.com>
2020-09-04 17:31:48 +0200

    Update CUKIERKI.cpp

--- CUKIERKI.cpp
+++ CUKIERKI.cpp
@@ -2,28 +2,20 @@
 using namespace std;
 int main ()
 {
-	int uczniowie; int wszystkiecukierki; int cukierkidlakazdego; int cukierkidlaJasia; 
-	cout<<"Podaj liczbe wszystkich uczniow w klasie Jasia: ";cin>>uczniowie;
-	int uczniowiebezJasia=uczniowie-1;
-	cout<<"Podaj liczbe wszystkich cukierkow: ";cin>>wszystkiecukierki;
-	if (uczniowiebezJasia>wszystkiecukierki)
+	int students; int allCandies; int candiesForEach; int candiesForJohn; 
+	cout<<"How many students does John's class count?: ";cin>>students;
+	int studentsMinusJohn=students-1;
+	cout<<"How many candies are there?: ";cin>>allCandies;
+	if (studentsMinusJohn>allCandies)
 	{
-		cout<<"Jest wiecej uczniow, niz cukierkow! Nikt nie dostanie nawet po jednym.";
+		cout<<"There are more students than candies. Nobody will get even one candy.";
 		return 0;
 	}
 	else
 	{
-	cukierkidlakazdego=wszystkiecukierki/uczniowiebezJasia;
-	cukierkidlaJasia=wszystkiecukierki-(uczniowiebezJasia*cukierkidlakazdego);
-	if (cukierkidlaJasia==1)
-	{
-		cout<<"Dla Jasia na wieczor zostal "<<cukierkidlaJasia<<" cukierek.";
-	}
-		else
-		{
-		cout<<"Dla Jasia na wieczor zostalo "<<cukierkidlaJasia<<" cukierkow.";
-	}
-}
-	
+	candiesForEach=allCandies/studentsMinusJohn;
+	candiesForJohn=allCandies-(studentsMinusJohn*candyForEach);
+	cout<<"John will have "<<candiesForJohn<<" candies for tonight.";
+	}	
 return 0;
 }
